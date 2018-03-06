60953cf41a4c3cd472675cdf27bd8724d32f3f98
Raji0476 <rajyam.rajireddy76@gmail.com>
2018-03-06 15:55:09 -0500

    added new code in java file

--- Game.java
+++ Game.java
@@ -1,5 +1,15 @@
+import java.io.Console;
 class Game {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       System.out.println("Welcome to the capitals name");
+       Console console = System.console();
+       String answer = console.readLine("What is the Capital of Australia?");
+       String capital = "Sydney";
+       if (answer.equals(capital)) {
+           System.out.println("Correct!");
+         }
+       else {
+           System.out.println("No, the answer is" +capital);
+         }
     }
 }
